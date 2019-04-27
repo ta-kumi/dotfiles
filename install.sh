36bbef850ad378ccffe29cc7cb0cb95d3a326b60
ta-kumi <33344708+ta-kumi@users.noreply.github.com>
2019-04-27 23:31:46 +0900

    neovim対応（vimと共通設定）

--- install.sh
+++ install.sh
@@ -8,3 +8,10 @@ do
 
     ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
 done
+
+# neovim対応
+if [[ ! -e ${HOME}/.config/nvim/ ]]; then
+	mkdir -p ${HOME}/.config/nvim/
+fi
+ln -snfv ${HOME}/.vim ${HOME}/.config/nvim/
+ln -snfv ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
